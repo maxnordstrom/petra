e3a5844acb7da6f61023af5c1f7a107f4363289f
Max Nordström <brev@maxnordstrom.se>
2019-01-16 00:55:29 +0100

    adding bootstrap and working on products.js

---
+++ js/products.js
@@ -0,0 +1,64 @@
+// Making the product objects
+
+let elcykelAllegro = {
+  artNumber: 39533028,
+  artName: 'Elcykel Allegro',
+  description: 'En smart och tillförlitlig elcykel för dam från Batavus utmärkt både för långa och kortare turer.',
+  category: 'Sport',
+  priceTax: 18499
+};
+
+let lapierreOvervolt = {
+  artNumber: 40266837,
+  artName: 'Lapierre Overvolt Urban 300',
+  description: 'Praktisk och bekväm elcykel med upprätt körställning.',
+  category: 'Sport',
+  priceTax: 20990
+};
+
+let chromeCast = {
+  artNumber: 'p35372817',
+  artName: 'Chrome cast 2nd generationen',
+  description: 'Visar film eller annan media från mobilen på TV:n',
+  category: 'Hemelektronik',
+  priceTax: 390
+};
+
+let appleTv = {
+  artNumber: 35552289,
+  artName: 'Apple TV 64GB 4th generation',
+  description: 'Nu kommer App Store till din tv. Du kan förvänta dig massor av spännande spel.',
+  category: 'Hemelektronik',
+  priceTax: 1990
+};
+
+let bigFoot = {
+  artNumber: 40151785,
+  artName: 'Big Foot truck 2wd',
+  description: 'Det här är bilen som startade alltihop och skapade den idag enorma monster-truck trenden.',
+  category: 'Leksaker',
+  priceTax: 2795
+};
+
+
+// Making an array of objects
+
+let petraProducts = [elcykelAllegro, lapierreOvervolt, chromeCast, appleTv, bigFoot];
+
+
+// Making the objects to arrays
+
+let elcykelAllegroArr = Object.entries(elcykelAllegro);
+let lapierreOvervoltArr = Object.entries(lapierreOvervolt);
+let chromeCastArr = Object.entries(chromeCast);
+let appleTvArr = Object.entries(appleTv);
+let bigFootArr = Object.entries(bigFoot);
+
+
+// Looping one of the entries
+
+for (let [prop, value] of bigFootArr) {
+  if (prop == 'category' && value == 'Leksaker') {
+  	console.log(bigFoot);
+  }
+}
